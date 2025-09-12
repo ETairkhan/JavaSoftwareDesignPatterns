fc50909144e99fd5384db45ca38aa6a798033406
ETairkhan <trouzerop@gmail.com>
2025-09-12 11:17:08 +0500

    Assignment1

--- Assignment1/src/CinemaBuilder.java
+++
@@ -1,5 +0,0 @@
-public interface CinemaBuilder {
-    void setDuration(int duration);
-    void setTitle(String title);
-    Cinema build();
-}
--- Assignment1/src/DirectCinemaBuilder.java
+++
@@ -1,7 +0,0 @@
-public class DirectCinemaBuilder {
-    public Cinema constract(CinemaBuilder builder){
-        builder.setDuration(10);
-        builder.setTitle("MARVEL");
-        return builder.build();
-    }
-}
