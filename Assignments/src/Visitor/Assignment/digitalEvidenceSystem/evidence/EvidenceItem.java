de913f00d1fa76d02ea2c2ca5abe0328d86f4e31
ETairkhan <trouzerop@gmail.com>
2025-11-07 12:10:42 +0500

    Visitor pattern

---
+++ Assignments/src/Visitor/Assignment/digitalEvidenceSystem/evidence/EvidenceItem.java
@@ -0,0 +1,8 @@
+package Visitor.Assignment.digitalEvidenceSystem.evidence;
+
+public interface EvidenceItem {
+    String getCaseNumber();
+    String getEvidenceId();
+    String getOfficerId();
+    boolean
+}
